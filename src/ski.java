ed88b2f581beebb6c600f7edbd7088fb1e0fdfc8
lch <lch920619@gmail.com>
2017-12-01 13:35:36 +0800

    fixed updating logic for drop, add comments

--- src/ski.java
+++ src/ski.java
@@ -31,15 +31,20 @@ public class ski {
             		maxDrop = Math.max(drop[i][j],maxDrop);
             	else if (maxPath == tmp)
             		maxDrop = drop[i][j];
+            	
+            	//update maxpath when necessary
             	maxPath = Math.max(maxPath, tmp);
         
             }
          }
         
+        //output results
         System.out.println(maxPath);
         System.out.println(maxDrop);
         System.out.print("\n");
       
+        
+        //debugging code
         /*for(int[] i:drop) {
         	for(int j: i)
         		System.out.print(j+" ");
@@ -55,24 +60,35 @@ public class ski {
 
 	}
 	
-	
+	//search method for point [row] [column]
 	static int computePath(int[][] map, int[][] path, int[][]drop, int row ,int column) {
-		
+		//check if point has already been checked, if so, direcly return result.
 		if (path[row][column] != 0) 
 			return path[row][column];
 
+		//initialize path and drop
 		int maxPath = 1;
 		int maxDrop = (drop[row][column]==-1)? 0:drop[row][column];
 		
+		//Traverse 4 adjacent points
 		for (int[] dir : dirs) {
 			int x = column + dir[0], y = row + dir[1];
+			//if point is out of bound, skip
 			if (x < 0 || x >= map[0].length || y < 0 || y >= map.length || map[y][x] >= map[row][column])
 				continue;
+			
+			//call method recursively
 			int len = 1 + computePath(map, path, drop, y, x);
 
-			if (maxPath <= len) {
+			//update max drop when necessary, take max when new path length = current path length,
+			//change to new number when new path length> current path lenght
+			if (maxPath == len) {
 				maxDrop = Math.max(map[row][column]-map[y][x]+drop[y][x], maxDrop);
+			}else if (maxPath < len) {
+				maxDrop = map[row][column]-map[y][x]+drop[y][x];
 			}
+			
+			//update max path when necessary
 			maxPath = Math.max(maxPath, len);
 			
 		}
