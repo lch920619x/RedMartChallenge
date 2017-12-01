a9f6c6d0b9e98a090e9e096415c8855eb51d9ff5
lch <lch920619@gmail.com>
2017-12-01 13:11:25 +0800

    finished working code

--- src/ski.java
+++ src/ski.java
@@ -4,52 +4,72 @@ public class ski {
 	public static final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-        int row = in.nextInt();
-        int column = in.nextInt();
+		Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int column = scan.nextInt();
         int[][] map = new int[row][column];
         int[][] drop = new int[row][column];
         int[][] path = new int[row][column];
-        int max = 1;
+        int maxPath = 1;
+        int maxDrop = 0;
         for (int i = 0; i<row; i++) {
             for (int j = 0; j<column; j++) {
-            	map[i][j] = in.nextInt();
-            	drop[i][j] = 0;
+            	map[i][j] = scan.nextInt();
+            	drop[i][j] = -1;
             	path[i][j] = 0;
             }
         }
+        scan.close();
         
         for (int i = 0; i<row; i++) {
             for (int j = 0; j<column; j++) {
-            	max = Math.max(max, computePath(map, path, drop, i, j));
+            	int tmp = computePath(map, path, drop, i, j);
+            	maxDrop = (maxPath <= tmp)? drop[i][j]:maxDrop;
+            	maxPath = Math.max(maxPath, tmp);
+
             }
         }
-        System.out.println(max);
+        System.out.println(maxPath);
+        System.out.println(maxDrop);
         System.out.print("\n");
-/*        for(int[] i:path) {
+      
+        /*for(int[] i:drop) {
         	for(int j: i)
         		System.out.print(j+" ");
     		System.out.print("\n");
-        }/*///
+        }
+       for(int[] i:path) {
+       	for(int j: i)
+       		System.out.print(j+" ");
+   		System.out.print("\n");
+       }*/
         
-        
+
 	}
 	
 	
 	static int computePath(int[][] map, int[][] path, int[][]drop, int row ,int column) {
-		if (path[row][column] != 0)
-			return path[row][column];
 		
-		int max = 1;
+		if (path[row][column] != 0) 
+			return path[row][column];
+
+		int maxPath = 1;
+		int maxDrop = (drop[row][column]==-1)? 0:drop[row][column];
 		
 		for (int[] dir : dirs) {
 			int x = column + dir[0], y = row + dir[1];
 			if (x < 0 || x >= map[0].length || y < 0 || y >= map.length || map[y][x] >= map[row][column])
 				continue;
 			int len = 1 + computePath(map, path, drop, y, x);
-			max = Math.max(max, len);
+
+			if (maxPath <= len) {
+				maxDrop = Math.max(map[row][column]-map[y][x]+drop[y][x], maxDrop);
+			}
+			maxPath = Math.max(maxPath, len);
+			
 		}
-		path[row][column] = max;
+		path[row][column] = maxPath;
+		drop[row][column] = maxDrop;
 		return path[row][column];
 	}
 
